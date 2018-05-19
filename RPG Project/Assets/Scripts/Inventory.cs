e8017fddc2e88c68a65b1445536825bc7567da2f
Terrence Chan <roxes2112@yahoo.com>
2018-05-19 11:03:17 +0800

    *Added inventory data structure for item.
    *Added item blueprints.
    
    *Added pickup item function.

---
+++ RPG Project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+
+    public static Inventory Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("More than one instances found!");
+            return;
+        }
+        Instance = this;
+    }
+
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged OnItemChangedCallBack;
+
+    public int space = 20;
+
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item _item)
+    {
+        if (!_item.isDefaultItem)
+        {
+            if (items.Count >= space)
+            {
+                Debug.Log("Not enough space!");
+                return false;
+            }
+
+            items.Add(_item);
+
+            if (OnItemChangedCallBack != null)
+            {
+                OnItemChangedCallBack.Invoke();
+            }
+        }
+
+        return true;
+    }
+
+    public void Remove(Item _item)
+    {
+        items.Remove(_item);
+
+        if (OnItemChangedCallBack != null)
+        {
+            OnItemChangedCallBack.Invoke();
+        }
+    }
+}
